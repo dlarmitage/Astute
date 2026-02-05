a9ccf3e1f3f9d2edb9143df3fe6412b5efa9f9fb
David L Armitage <dlarmitage@gmail.com>
2026-02-05 07:51:05 -0700

    Initial Commit

---
+++ AstuteTests/AstuteTests.swift
@@ -0,0 +1,16 @@
+//
+//  AstuteTests.swift
+//  AstuteTests
+//
+//  Created by David Armitage on 2/5/26.
+//
+
+import Testing
+
+struct AstuteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
